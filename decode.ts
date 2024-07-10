525518aeb646c1f61948bc87ae8d989646215fba
Ali Can KANBOLAT <kanbolat.alican@gmail.com>
2024-07-10 09:42:03 +0300

    homework project completed

---
+++ decode.ts
@@ -0,0 +1,6 @@
+import bs58 from "bs58";
+
+const kp = bs58.decode("2cwNbM38zXX8iPN6h9wWX93tJaHZoH9zNAJTKLAk21AJG9c33yabBrTnAwcURA5J4ktdfUtxyYZjkHva914wfbkD")
+
+
+console.log("secretkey_bs58formatted:::", kp)
